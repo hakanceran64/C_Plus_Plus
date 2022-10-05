e0267834ee6e5d63ee314fd5bee38bd13eaeaa29
Hakan CERAN <hakanceran64@gmail.com>
2022-10-05 09:12:56 +0300

    Arrays updated

--- Fundamentals/Arrays/arrays.cpp
+++ Fundamentals/Arrays/arrays.cpp
@@ -13,16 +13,37 @@ using namespace std;
 
 int main(){
 
-    // An Array
-    int myNumbers[4];
+    // ## Array Declaration and Allocation
+    int myNumbers[4]; // integer array with 4 elements
     string cars[4] = {"Volvo", "BMW", "Ford", "Audi"};
+    int myArray[] = {1, 2, 3};
 
-    // Access the Element of an Array
-    cout << cars[3] << endl;
+    // ### Access the Element of an Array
+    cout << "Access the Element of an Array cars[3]: " << cars[3] << endl;
 
-    // Change an Array Element
+    // ### Change an Array Element
     cars[1] = "Renault";
-    cout << cars[1] << endl;
+    cout << "Change an Array Element cars[1]: " << cars[1] << endl;
+
+    // ## Multi-dimensional Arrays
+    int myMultiArray[2][2] = {
+        {0, 1},
+        {2, 3}
+    };
+
+    // ## Dynamics Arrays
+    int* p = new int[3]; // dynamically allocated array
+    *(p+0) = 10;
+    cout << "Dynamics Arrays p[0]: " << p[0] << endl;
+
+    // Determining Array Size
+    int length = sizeof(myArray) / sizeof(int); // 3
+    cout << "myArray Size: " << length << endl;
+
+    // When you are done using dynamic array you must remember to delete it.
+    int size = 3;
+    int* pArray = new int[size];
+    delete[] pArray; // Release Allocated Array
 
     return 0;
 }
