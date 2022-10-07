2d0f7bc5a0015c1041b3023cce07f101d6f3cdf4
Hakan CERAN <hakanceran64@gmail.com>
2022-10-07 23:31:54 +0300

    Polymorphism

---
+++ Fundamentals/OOP/Polymorphism/AnimalSound/AnimalSound.cpp
@@ -0,0 +1,48 @@
+/*
+	@FileName: AnimalSound.cpp
+	@Author: Hakan CERAN
+	@Date: 07/10/2022 23:31
+	@Description: Polymorphism example.
+	@Notes: 
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Base class
+class Animal{
+    public:
+        void animalSound(){
+            cout << "The animal makes a sound!" << endl;
+        }
+};
+
+// Derived class
+class Cat : public Animal {
+    public:
+        void animalSound(){
+            cout << "The cat says: meow meow" << endl;
+        }
+};
+
+// Derived class
+class Dog : public Animal {
+    public:
+        void animalSound(){
+            cout << "The dog says: bow wow" << endl;
+        }
+};
+
+int main(){
+    Animal myAnimal;
+    Cat myCat;
+    Dog myDog;
+
+    myAnimal.animalSound();
+    myCat.animalSound();
+    myDog.animalSound();
+
+    return 0;
+}
